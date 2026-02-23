b4b0e37ab16383a60d95705190490ef00102b752
VladislavZayt <vladislav-zaytsev-90@mail.ru>
2026-02-23 20:37:49 +0300

    Bug fix

--- markup_ml/app/core/random_search_combinations.py
+++ markup_ml/app/core/random_search_combinations.py
@@ -6,28 +6,37 @@ def random_search_params(param_ranges: Dict[str, Tuple], n_iter: int = 10) -> Li
         return []
 
     combinations = []
+
+
     for _ in range(n_iter):
         combo = {}
-        for key, params in param_ranges.items():
-            if len(params) == 0:
-                continue
+        unique_flag = False
+        generate_count = 0
+        while not unique_flag and generate_count < 10:
+            for key, params in param_ranges.items():
+                if len(params) == 0:
+                    continue
 
-            if len(params) == 1:
-                combo[key] = params[0]
-            else:
-                if isinstance(params[0], int) and isinstance(params[1], int):
-
-                    combo[key] = random.randint(params[0], params[1])
-                elif isinstance(params[0], float) or isinstance(params[1], float):
-
-                    combo[key] = random.uniform(params[0], params[1])
-
-                elif isinstance(params[0], str) or isinstance(params[1], str):
-                    combo[key] = params[random.randint(0, len(params) - 1)]
-
+                if len(params) == 1:
+                    combo[key] = params[0]
                 else:
-                    combo[key] = random.uniform(float(params[0]), float(params[1]))               
+                    if isinstance(params[0], int) and isinstance(params[1], int):
+
+                        combo[key] = random.randint(params[0], params[1])
+                    elif isinstance(params[0], float) or isinstance(params[1], float):
+
+                        combo[key] = random.uniform(params[0], params[1])
+
+                    elif isinstance(params[0], str) or isinstance(params[1], str):
+                        combo[key] = params[random.randint(0, len(params) - 1)]
+
+                    else:
+                        combo[key] = random.uniform(float(params[0]), float(params[1]))               
             
-        combinations.append(combo)
+            generate_count += 1
+
+            if combo not in combinations:
+                combinations.append(combo)
+                unique_flag = True
 
     return combinations
