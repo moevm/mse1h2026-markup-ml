8e1ad921be3398bf481c76bd5475606ba6c4d39b
Ivan Anenkov <144446772+vanyanenkov@users.noreply.github.com>
2026-02-25 01:05:17 +0300

    Update main.py

--- markup_ml/main.py
+++ markup_ml/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI(
     title="AutoML YOLO API",
@@ -15,19 +17,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#Task 1.1.4
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-# app.mount("/runs", StaticFiles(directory="runs"), name="runs")
 
-#Task 1.1.3
 @app.get("/ping")
 async def ping():
     return {"status": "ok"}
 
-@app.get("/")
-async def root():
-    return {
-        "message": "AutoML YOLO API is running",
-        "docs": "/docs",
-        "test_endpoint": "/ping"
-    }
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
